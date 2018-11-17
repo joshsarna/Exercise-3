a40f317eef6c17363cff4655222b248ab772a4a9
joshsarna <joshsarna@gmail.com>
2018-11-16 19:12:49 -0800

    finish exercise 3

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+/* global Vue */
+
 new Vue({
-        el: '#exercise',
-        data: {
-            value: 0
-        }
-    });
+  el: '#exercise',
+  data: {
+    value: 0
+  },
+
+  computed: {
+    result: function() {
+      return this.value > 37 ? 'Done!' : 'Not there yet!';
+    }
+  },
+
+  watch: {
+    value: function(value) {
+      var vm = this;
+      setTimeout( function() {
+        vm.value = 0;
+      }, 5000);
+    }
+  }
+});
